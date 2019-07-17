046fa5321c12a92ddd47fd40f48d885e5eea7285
Zachery Wagner <zacherywagner@gmail.com>
2019-07-16 21:32:15 -0400

    Created Project

--- ARDemos/AppDelegate.swift
+++ ARDemos/AppDelegate.swift
@@ -12,10 +12,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navController: UINavigationController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let mainViewController = SingleNodeViewController()
+        navController = UINavigationController(rootViewController: mainViewController)
+        navController.isNavigationBarHidden = true
+        navController.delegate = self
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
@@ -40,7 +49,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
+extension AppDelegate: UINavigationControllerDelegate {
+
+}
---
+++ ARDemos/RealityView.swift
@@ -0,0 +1,16 @@
+//
+//  RealityView.swift
+//  ARObjectRendering
+//
+//  Created by Zachery Wagner on 7/16/19.
+//  Copyright © 2019 Zachery Wagner. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import UIKit
+
+class RealityView {
+    
+}
+
