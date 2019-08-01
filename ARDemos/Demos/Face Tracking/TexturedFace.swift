a450b0f4d8afd92eea7ba19187247b3aef198199
Zachery Wagner <zacherywagner@gmail.com>
2019-08-01 16:23:42 -0400

    Cleanup

--- ARDemos/Demos/Face Tracking/TexturedFace.swift
+++ ARDemos/Demos/Face Tracking/TexturedFace.swift
@@ -16,18 +16,15 @@ class TexturedFace: NSObject, VirtualContentController {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         guard let sceneView = renderer as? ARSCNView,
             anchor is ARFaceAnchor else { return nil }
-        
-        #if targetEnvironment(simulator)
-        #error("ARKit is not supported in iOS Simulator. Connect a physical iOS device and select it as your Xcode run destination, or select Generic iOS Device as a build-only destination.")
-        #else
+
         let faceGeometry = ARSCNFaceGeometry(device: sceneView.device!)!
         let material = faceGeometry.firstMaterial!
-        
-        material.diffuse.contents = #imageLiteral(resourceName: "wireframeTexture") // Example texture map image.
+
+
+        material.diffuse.contents = SKTexture(imageNamed: "wireframeTexture")
         material.lightingModel = .physicallyBased
         
         contentNode = SCNNode(geometry: faceGeometry)
-        #endif
         return contentNode
     }
     
